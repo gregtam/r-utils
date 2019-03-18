021916e25aa1a125cdf9f5e5121f703e279699f7
gregtam <gregtam1991@gmail.com>
2019-03-18 08:34:00 -0400

    Add save_tbl_sql_to_db()

--- r_utils.R
+++ r_utils.R
@@ -1,5 +1,6 @@
 library(stringr)
 library(tibble)
+library(dbplyr)
 
 date_file_path <- function(file_path, directory) {
   # Today's date represented as a string
@@ -38,4 +39,21 @@ transpose_numeric_tibble <- function(data_tbl, numeric_col_name) {
               !!numeric_col_name := V1)
 
   return(data_t_tbl)
+}
+
+save_tbl_sql_to_db <- function(data_tbl_sql, table_name, impala, schema = '') {
+  # Saves a tbl_sql to database
+
+  # String representation of the SELECT command that corresponds to tbl_sql
+  select_sql_str <- sql_render(data_tbl_sql) %>% as.character
+
+  if (nchar(schema) > 0) {
+    table_name <- paste0(schema, '.', table_name)
+  }
+
+  # SQL Query string to create the table
+  create_table_str <- paste('CREATE TABLE', table_name, 'AS', select_sql_str)
+
+  # Execute query in Impala
+  dbExecute(impala, create_table_str)
 }
